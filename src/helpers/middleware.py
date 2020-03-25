8df29d512127fbe9d4f601945188c61db1e87744
alimoslehirad <alimoslehirad@gmail.com>
2020-03-25 16:39:40 +0430

    Update middleware.py

--- src/helpers/middleware.py
+++ src/helpers/middleware.py
@@ -5,7 +5,7 @@ import sys
 
 REQUEST_COUNT = Counter(
     'request_count', 'App Request Count',
-    ['app_name', 'method', 'endpoint', 'http_status' , 'ENTRYPOINT']
+    ['app_name', 'method', 'endpoint', 'http_status' ]
 )
 REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency',
     ['app_name', 'endpoint']
